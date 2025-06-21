9caa3dfd1f419508da1608c7594fa72966736d08
Altamash Rafiq <altamashrafiq@gmail.com>
2025-06-21 22:43:40 +0500

    painstakingly convert ChatMessage component to tsx

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,9 +1,16 @@
+import { isValidElement } from "react";
 import Markdown from "react-markdown";
 import RobotProfileImage from "../assets/robot.png";
 import UserProfileImage from "../assets/ammara.jpg";
 import "./ChatMessage.css";
 
-export function ChatMessage({ message, sender, time }) {
+type ChatMessageProps = {
+  message: string | React.ReactElement;
+  sender: string;
+  time: string;
+};
+
+export function ChatMessage({ message, sender, time }: ChatMessageProps) {
   return (
     <div
       className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
@@ -12,7 +19,7 @@ export function ChatMessage({ message, sender, time }) {
         <img src={RobotProfileImage} className="chat-message-profile" />
       )}
       <div className="chat-message-text">
-        {message.type === "img" ? (
+        {isValidElement(message) ? (
           message
         ) : (
           <div className="chat-message-markdown">
